1079fb717864bf2a83373db449c190e50edc05b9
Attilio <34461288+attgua@users.noreply.github.com>
2021-06-05 21:48:39 +0200

    first

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "GRANDE HAI VINTO! NUOVA PARTITA?";
+    public static final String ERROR = "ERROREEE!!";
+}
